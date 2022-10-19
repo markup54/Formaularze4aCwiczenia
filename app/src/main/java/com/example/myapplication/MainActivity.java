9d6bd62a575835202283ea4d7a16e41e5b6504ff
markup54 <marzenakupka@tlen.pl>
2022-10-19 08:37:35 +0200

    zmiana w edit text

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,16 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ImageView obrazekImageView;
     EditText numberEditText;
+    ArrayList <Integer> obrazki = new ArrayList<>();
+    int indeksAktualny = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        obrazki.add(R.drawable.rys1);
+        obrazki.add(R.drawable.rys2);
+        obrazki.add(R.drawable.rys3);
+        obrazki.add(R.drawable.rys4);
         obrazekImageView =findViewById(R.id.imageView);
         numberEditText = findViewById(R.id.editTextNumber);
         numberEditText.addTextChangedListener(
@@ -25,8 +35,21 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+                    public void onTextChanged(CharSequence charSequence,
+                                              int i, int i1, int i2) {
+                        Toast.makeText(MainActivity.this,
+                                charSequence, Toast.LENGTH_SHORT)
+                                .show();
+                        //charSequence przechowuje wartość
+                        // zapisaną w edittekscie
+                        if(charSequence.toString().length()>0) {
+                            indeksAktualny = Integer.valueOf(charSequence.toString());
+                            if (indeksAktualny < 0 || indeksAktualny >= obrazki.size()) {
+                                indeksAktualny = 0;
+                            }
+                            obrazekImageView
+                                    .setImageResource(obrazki.get(indeksAktualny));
+                        }
                     }
 
                     @Override
